b918d39327a55cbdd1086e373893736d1403a755
Torben Knerr <mail@tknerr.de>
2017-05-30 16:41:14 +0200

    generate initial structure for chef cookbook / ansible role

---
+++ myapp_chef/README.md
@@ -0,0 +1,4 @@
+# myapp_chef
+
+TODO: Enter the cookbook description here.
+
---
+++ myapp_chef/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: myapp_chef
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
