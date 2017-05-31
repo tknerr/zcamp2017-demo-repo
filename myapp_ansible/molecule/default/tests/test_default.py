e708df53d648f43b98c5dd03bed2cf9c19ab729a
Torben Knerr <mail@tknerr.de>
2017-05-31 08:33:33 +0200

    recreated molecule scenario with vagrant driver, and added test for apache2 package

--- myapp_ansible/molecule/default/tests/test_default.py
+++ myapp_ansible/molecule/default/tests/test_default.py
@@ -1,14 +1,3 @@
-import os
 
-import testinfra.utils.ansible_runner
-
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
-
-
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_apache2_webserver_is_installed(host):
+    assert host.package('apache2').is_installed
